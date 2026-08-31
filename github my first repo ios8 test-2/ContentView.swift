b004c390e8b7ed1076dbc6907c9b28e4752afbc4
yungtat <soh.yung.tat.s2026.ssts.edu.sg>
2026-08-31 16:09:46 +0800

    Initial Commit

---
+++ github my first repo ios8 test-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github my first repo ios8 test-2
+//
+//  Created by Soh Yungtat on 31/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github my first repo ios8 test-2/github_my_first_repo_ios8_test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  github_my_first_repo_ios8_test_2App.swift
+//  github my first repo ios8 test-2
+//
+//  Created by Soh Yungtat on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct github_my_first_repo_ios8_test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
